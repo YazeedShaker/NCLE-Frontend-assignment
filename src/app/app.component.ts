8e0634274a248b8e7befc0372b8762fba6bb82f4
YazeedShaker <yazeed95.shaker@gmail.com>
2025-04-14 08:08:10 +0200

    initialized the translation with a default language  in the app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HeaderComponent } from './shared-components/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, TranslateModule, HeaderComponent],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private translateService: TranslateService) {
+    this.initTranslation();
+  }
+
+  private initTranslation() {
+    this.translateService.setDefaultLang('en');
+    this.translateService.use('en');
+  }
 }
